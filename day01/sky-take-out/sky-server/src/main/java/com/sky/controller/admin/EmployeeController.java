437711bf81cc68eee0b76773ab4ad0f54e97cd7d
buhuishiwo <1437317989@qq.com>
2025-05-08 21:48:33 +0800

    Auto commit at 2025/05/08 周四 21:48:32.92

--- day01/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ day01/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -74,6 +75,14 @@ public class EmployeeController {
     @PostMapping("/logout")
     @ApiOperation("员工登出接口")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    @PostMapping
+    @ApiOperation("添加员工接口")
+    public Result save(@RequestBody EmployeeDTO employee) {
+        log.info("新增员工：{}", employee);
+        employeeService.save(employee);
         return Result.success();
     }
 
